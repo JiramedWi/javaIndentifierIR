bb935c7b9eb063769360152dccb3e116b296b6db
Jiramed_Withunsapphasiri <peepeedojin@gmail.com>
2022-11-15 10:27:05 +0700

    tf & tfidf multiIndex

--- tf.py
+++ tf.py
@@ -1,4 +1,3 @@
-import pandas
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -6,22 +5,40 @@ from main import cleandata
 from main import preprocess_stem
 import nltk
 
-# nltk.download('punkt')
-# nltk.download('stopwords')
-methodSetup = 'resource/result_version_1/clean_data/methodNoSetup_concat_clean.csv'
-methodSetup_df = pd.read_csv(methodSetup, header=0, index_col=0)
-methodSetup_df_setup = methodSetup_df['setup'].fillna('')
-methodSetup_df_method = methodSetup_df['methodNoSetup'].fillna('')
-dict = pd.concat([methodSetup_df_setup])
+'''When you new of this project, run this below first'''
+'''
+nltk.download('punkt')
+nltk.download('stopwords')
+'''
 
-cv = CountVectorizer()
-cv = cv.fit(dict)
+filepath = 'resource/i_think_should_be_the_last_time/CleanDetailDataframe.pkl'
+df = pd.read_pickle(filepath)
+multiIndex_df = df.set_index(['class', 'method'])
+setup = multiIndex_df.xs('setUp', level='method')
+method = multiIndex_df.loc[multiIndex_df.index.get_level_values(1) != 'setUp']
+detail_setup = setup['detail']
+detail_method = method['detail']
+# setup = df.groupby(['class'],['method']).
+# methodSetup = 'resource/result_version_1/clean_data/methodNoSetup_concat_clean.csv'
+# methodSetup_df = pd.read_csv(methodSetup, header=0, index_col=0)
+# methodSetup_df_setup = methodSetup_df['setup'].fillna('')
+# methodSetup_df_method = methodSetup_df['methodNoSetup'].fillna('')
+# dict = pd.concat([methodSetup_df_setup])
+# dict = detail
+#
 
-# X_fit_setup = cv.transform(methodSetup_df_setup)
-X_fit_method = cv.transform(methodSetup_df_method)
-
+cv = CountVectorizer(token_pattern=r'(?u)\b\w+\b')
+'''Create term with all word as a term'''
+cv = cv.fit(multiIndex_df['detail'])
+#
+X_fit_setup = cv.transform(detail_setup)
+X_fit_method = cv.transform(detail_method)
+X_fit_all = cv.transform(multiIndex_df['detail'])
+#
 cv_feature_name = cv.get_feature_names_out()
-# setup_tf = pd.DataFrame(X_fit_setup.toarray(), columns=cv_feature_name)
+setup_tf = pd.DataFrame(X_fit_setup.toarray())
 method_tf = pd.DataFrame(X_fit_method.toarray(), columns=cv_feature_name)
+all_method_tf = pd.DataFrame(X_fit_all.toarray())
+all_method_tf = all_method_tf.set_index([multiIndex_df.index.get_level_values(0),multiIndex_df.index.get_level_values(1)])
 # print(setup_tf)
-print(method_tf)
+# print(method_tf)
--- tf_idf.py
+++ tf_idf.py
@@ -5,12 +5,21 @@ from sklearn.metrics.pairwise import cosine_similarity
 import nltk
 from collections import defaultdict
 from gensim import corpora
-methodSetup = 'resource/result_version_1/clean_data/methodNoSetup_concat_clean.csv'
-methodSetup_df = pd.read_csv(methodSetup, header=0, index_col=0)
-methodSetup_df_setup = methodSetup_df['setup'].fillna('')
-methodSetup_df_method = methodSetup_df['methodNoSetup'].fillna('')
-dict = pd.concat([methodSetup_df_setup])
 
+filepath = 'resource/i_think_should_be_the_last_time/CleanDetailDataframe.pkl'
+df = pd.read_pickle(filepath)
+multiIndex_df = df.set_index(['class', 'method'])
+setup = multiIndex_df.xs('setUp', level='method')
+method = multiIndex_df.loc[multiIndex_df.index.get_level_values(1) != 'setUp']
+detail_setup = setup['detail']
+detail_method = method['detail']
+detail_all_method = multiIndex_df['detail']
+
+# methodSetup = 'resource/result_version_1/clean_data/methodNoSetup_concat_clean.csv'
+# methodSetup_df = pd.read_csv(methodSetup, header=0, index_col=0)
+# methodSetup_df_setup = methodSetup_df['setup'].fillna('')
+# methodSetup_df_method = methodSetup_df['methodNoSetup'].fillna('')
+# dict = pd.concat([methodSetup_df_setup])
 
 
 # vectorizer = TfidfVectorizer(preprocessor=preprocess_stem, use_idf=True)
@@ -18,13 +27,25 @@ dict = pd.concat([methodSetup_df_setup])
 # tfidf_df_stemmed = pd.DataFrame(vector_stemmed.toarray(), index=clean_df, columns=vectorizer.get_feature_names())
 
 
-vectorizer_no_stem = TfidfVectorizer(use_idf=True)
-vectorizer_no_stem = vectorizer_no_stem.fit(dict)
-vector_no_stem = vectorizer_no_stem.fit_transform(methodSetup_df_method)
-tfidf_df_no_stem = pd.DataFrame(vector_no_stem.toarray(), index=methodSetup_df_method, columns=vectorizer_no_stem.get_feature_names())
-tfidf_df_no_stem.shape
-print(tfidf_df_no_stem)
-tfidf_df_no_stem.to_csv('resource/result_version_1/tf_idf/tfidf_df_no_stem.csv', encoding='utf-8')
+vectorizer_no_stem = TfidfVectorizer(use_idf=True, token_pattern=r'(?u)\b\w+\b')
+vectorized_no_stem = vectorizer_no_stem.fit(detail_all_method)
+vector_no_stem_setup = vectorized_no_stem.transform(detail_setup)
+vector_no_stem_methods = vectorized_no_stem.transform(detail_method)
+vector_no_stem_all = vectorized_no_stem.transform(detail_all_method)
+
+vector_feature_name = vectorized_no_stem.get_feature_names_out()
+
+tfidf_df_no_stem_method = pd.DataFrame(vector_no_stem_methods.toarray(), columns=vectorized_no_stem.get_feature_names_out())
+tfidf_df_no_stem_method = tfidf_df_no_stem_method.set_index(
+    [method.index.get_level_values(0), method.index.get_level_values(1)])
+tfidf_df_no_stem_all = pd.DataFrame(vector_no_stem_all.toarray(), columns=vectorized_no_stem.get_feature_names_out())
+tfidf_df_no_stem_all = tfidf_df_no_stem_all.set_index(
+    [multiIndex_df.index.get_level_values(0), multiIndex_df.index.get_level_values(1)])
+
+print(tfidf_df_no_stem_all)
+tfidf_df_no_stem_method.to_pickle("resource/i_think_should_be_the_last_time/tfidf_methods.pkl")
+tfidf_df_no_stem_all.to_pickle("resource/i_think_should_be_the_last_time/tfidf_all.pkl")
+# tfidf_df_no_stem.to_csv('resource/result_version_1/tf_idf/tfidf_df_no_stem.csv', encoding='utf-8')
 
 # To inspect the first document
 # first_vectorizer = vector[0]
@@ -41,4 +62,3 @@ tfidf_df_no_stem.to_csv('resource/result_version_1/tf_idf/tfidf_df_no_stem.csv',
 #     # print('Number of unique tokens: %d' % len(dict))
 #     # print('Number of document: %d' % len(corpus))
 #     return corpus, dict
-
